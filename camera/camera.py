9dccc344630c9619d71e81b8d7f2280f06dac8a7
Niko Raitanen <nraitanen@gmail.com>
2021-08-02 16:21:47 +0300

    Small script for acquiring training data via Raspberry Pi camera v2.

--- camera/camera.py
+++ camera/camera.py
@@ -1,8 +1,24 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
-camera.start_preview(alpha=192)
-sleep(1)
-camera.capture("/home/pi/Desktop/testpic.jpg")
+camera = PiCamera(sensor_mode=0)
+
+camera.iso = 100
+camera.resolution = (640, 480)
+camera.framerate = 2
+camera.rotation = 180
+camera.brightness = 40
+camera.exposure_mode = "auto"
+camera.zoom = (0, 0.32, 1, 0.48)
+camera.video_stabilization = True
+camera.start_preview()
+sleep(10)
 camera.stop_preview()
+
+i = 0
+while i < 10000:
+    sleep(0.5)
+    camera.capture("/home/pi/Desktop/frames/IMG_" + str(i) + ".jpg")
+    i += 1
+
+camera.close()
